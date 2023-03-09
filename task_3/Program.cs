7f8062526baf10d08fe43e700acca4407796325e
Rufat Karimov <ruffat_karimov@mail.ru>
2023-03-09 15:38:18 +0600

    third task

---
+++ task_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int m = 3;
+int n = 3;
+
+int[,] array = GetArray(m, n, 0, 10);
+
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+    
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        avarage = (avarage + array[i, j]);
+    }
+    avarage = avarage / m;
+    Console.Write($"{avarage:f1};  ");
+}
+Console.WriteLine();
